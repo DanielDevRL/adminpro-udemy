2edab14d0125c48693b94324c1b5ef6ea2e87758
Daniel Robledo <danieldevrl@outlook.com>
2018-09-26 17:42:52 -0500

    first commit

---
+++ src/app/login/resgiter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resgiter',
+  templateUrl: './resgiter.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class ResgiterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
